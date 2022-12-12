bdd8358f3376dd1962ea1d8a78e688ab72f4a25d
MrAFerreira <andre.f.v.ferreira@gmail.com>
2022-12-12 13:38:28 +0000

    update 1212

--- tests/theme_properties.test.js
+++
@@ -1,3 +0,0 @@
-const puppeteer = require('puppeteer');
-
-//Create test to check if the themes object has the provided values
--- tests/theme_setup.test.js
+++
@@ -1,4 +0,0 @@
-const puppeteer = require('puppeteer');
-
-//Create first test to check if ThemeProvider is created and applied on index.js
-//Also check if the package.json has been updated
